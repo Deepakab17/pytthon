79190a5f9e290b224c45a48944e766b209009785
Deepakab17 <deepaknab17@gmail.com>
2025-09-23 23:45:03 +0530

    commited

---
+++ al.py
@@ -0,0 +1,22 @@
+#s='deepak'
+#print(len(s))
+#print(max(s))
+#print(min(s))
+#print(type(s))
+#print(id(s))
+#print(s)
+x=[1,2,3,4]
+y=[1,2,3,4]
+#print(x+y)
+#print(x-y)
+#print(x*y)
+#x*=3
+#x+='language'
+#print(x==y)
+#print(x>y)
+#print(x<y)
+#print(x<=y)
+#print(x>=y)
+#print(x!=y)
+print(x is y)
+print(x is not y)
