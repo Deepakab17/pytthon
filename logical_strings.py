be9a4abb226f98ecfd2cbda283431fa7de2d52f3
Deepakab17 <deepaknab17@gmail.com>
2025-09-24 23:26:20 +0530

    methods and logical strings

---
+++ logical_strings.py
@@ -0,0 +1,11 @@
+s='java'
+y='python'
+r=''
+d='jack'
+k=''
+l=''
+print(s and y)
+print(s or y)
+print(r or d)
+print(k or d)
+print(not k)
---
+++ strongs_method.py
@@ -0,0 +1,11 @@
+#s='pyThon'
+#print (s.swapcase())
+#print (s.title())
+#a='my name is aman khan124545'
+#print(a.count(a))
+#print(s.count(''))
+#print(a.count('12'))
+j='java'
+p='python'
+#v=''.join(j,p)
+v=''.join([j,p])
