0358d7769be595ee69b3a9e7ce67472f12bc7343
Deepakab17 <deepaknab17@gmail.com>
2025-09-27 23:47:26 +0530

    commit this tuple

--- list_methods.py
+++ list_methods.py
@@ -1,11 +1,17 @@
-l=[10,20,30,'python']
-l1=[1,2,3,4]
-#l.extend(l1)
-#l.extend([98847,'sharvan'])
-#print(l)
-#print(l.pop())
-l2=l.copy
-#print(id(l1),id(l2))
-l1.sort(reverse=True)
-print(l1)
+# l=[10,20,30,'python']
+# l1=[1,2,3,4]
+# #l.extend(l1)
+# #l.extend([98847,'sharvan'])
+# #print(l)
+# #print(l.pop())
+# l2=l.copy
+# #print(id(l1),id(l2))
+# l1.sort(reverse=True)
+# print(l1)
 
+grade=input('enter your grades here (from A to D) :\n')
+grade =(grade,)
+if grade in('A', 'B', 'C', 'D'):
+    print(grade.count('A'))
+else :
+    print("invalid Grade : please Try again \n")                               
---
+++ tuple_class.py
@@ -0,0 +1,9 @@
+# tuple=(10,20,30,'python','java')
+# print(len(tuple))
+# print(type(tuple))
+# print(id(tuple))
+# t=eval(input('enter any value\n')
+d={'name':'deepak','age': 37}
+k={'name': 'sharvan','age': 24}
+print(d,type(d))
+print(id(d),id(k))
