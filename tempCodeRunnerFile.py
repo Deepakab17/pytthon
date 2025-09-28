aa6aebc2ab1fbaf8b6aceca8b05403d859cd459f
Deepakab17 <deepaknab17@gmail.com>
2025-09-28 23:54:24 +0530

    commit the tuple operators

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+# y=('1,2,5,')
+# print(t+y)
+t=()
+y=('20,chutki',)
+print(t and y)
+print(type(t))
---
+++ tuple_operators.py
@@ -0,0 +1,7 @@
+# t=('1,2,4,1,string,sony')
+# y=('1,2,5,')
+# print(t+y)
+t=()
+y=('20,chutki',)
+print(t and y)
+print(type(t))
