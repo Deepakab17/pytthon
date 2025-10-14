2ef46f103fb6511a3b91656e115a5868a184294c
Deepakab17 <deepaknab17@gmail.com>
2025-10-14 23:19:52 +0530

    for loop

---
+++ forloopsinpython.py
@@ -0,0 +1,23 @@
+# s=input('enter any string')
+# for i in s:
+#     print(i)
+# t=eval(input("enter any tuple"))
+# for i in t:
+#     print(i)
+# d=eval(input('enter key value pairs\n'))
+# for i in d.values():
+#     print(i)
+# n=int(input('enter any number\n'))
+# for i in range(1,n+1):
+#     print(''*i+'*'*(n-i))
+n=int(input('enter any number\n'))
+for i in range(0,n):
+    print(' '*i+' *'*(n-i))
+    for j in range(4,n+1)  :
+        print (' '*i+' *'*(n-i)) 
+
+
+
+
+
+
