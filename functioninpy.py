f120dec8f3cf5149e9544aee8b297ec3dab4510b
Deepakab17 <deepaknab17@gmail.com>
2025-10-30 21:58:22 +0530

    functions further

--- functioninpy.py
+++ functioninpy.py
@@ -2,7 +2,38 @@
 #     area=length*width
 #     print(area)
 # rect_area(14,10)
-def rect_area(length,width):
-    area=length*width
-rect_area(14,10)
-print(area)
+# unit= "feet Square"
+# area=500
+# def rect_area(length,width):
+#     area=length*width
+#     return area
+# m_area=rect_area(14,10)
+# print(m_area,unit)
+# print(area)
+# def greet(name):
+#     return "Hello "+ name +"!"
+# print(greet("Chutki"))
+# print(greet("Deepak"))
+# def add(a,b):
+#     return a+b
+# result=add(40,80)
+# print(add(result,80))
+# def compute(a,b):
+#     result=a*b
+#     if result > 10:
+#         return result -3
+#     else:
+#         return result +5
+# print(compute(5,4))
+# print(compute(3,2))
+# counter=70
+# def increment(value):
+#     counter+=value
+#     return counter
+# print(increment(10))
+# print(counter)
+def greet(name):
+    print(f"hi {name}")
+
+
+print(greet("deepak"))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+counter=70
+def increment(value):
+    counter+=value
+    return counter
+print(increment(10))
+print(counter)
