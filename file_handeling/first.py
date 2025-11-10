b11ad6cdaf2ee583dddbfc9a5dba852d5eeea5ae
Deepakab17 <deepaknab17@gmail.com>
2025-11-10 12:50:09 +0530

    file handeling

--- file_handeling/first.py
+++ file_handeling/first.py
@@ -20,14 +20,46 @@
 #                 class\n'''
 # f.writelines([cl,cl1,cl2])
 # f.close()
-f=open('a1.txt','r')
-# all_data=f.read(5)
-# # for x in all_data(5):
-#     # print(x)
-# print(all_data)
-# data=f.read()
+# f=open('a1.txt','r')
+# # all_data=f.read(5)
+# # # for x in all_data(5):
+# #     # print(x)
+# # print(all_data)
+# # data=f.read()
+# # print(data)
+# # data=f.readline()
+# # print(data)
+# data=f.readlines()
 # print(data)
-# data=f.readline()
-# print(data)
-data=f.readlines()
-print(data)
+# with open('a1.txt','a') as f:
+#     data="this is me, deepak"
+#     f.write(data)
+#     print(f.closed)
+# print(f.closed)
+# with open('a2.txt','r') as f:
+#     print(f.tell())
+#     f.seek(5)
+#     print(f.tell())
+#     data=f.read(5)
+#     print(data)
+#     print(f.tell())
+#     f.seek(-3,1)
+#     print(f.tell())
+# with open('a2.txt','rb') as f:
+#     print(f.tell())
+#     f.seek(5)
+#     print(f.tell())
+#     data=f.read(5)
+#     print(data)
+#     print(f.tell())
+#     f.seek(-3,1)
+#     print(f.tell())
+# with open('a1.txt','rb') as f:
+#     f.seek(-6,2)
+#     print(f.tell())
+#     print(f.read())
+
+
+
+
+    
