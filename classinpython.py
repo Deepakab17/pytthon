298730122692a44b4983727b2433bb0c59ad5cb8
Deepakab17 <deepaknab17@gmail.com>
2025-11-12 23:14:16 +0530

    Class

--- classinpython.py
+++ classinpython.py
@@ -1,16 +1,47 @@
-class Student:
-    pass
-print(id(Student))
-obj=Student
-print(id(obj))
-obj1=Student
-print(id(obj1))
+# class Student:
+#     pass
+# print(id(Student))
+# obj=Student
+# print(id(obj))
+# obj1=Student
+# print(id(obj1))
 # the memory address is same because it is not assign to other objects... the address belongs to the class
 
-class Student:
-    '''deepak'''
-    school='SHSS'
-    def showdetails():
-        print("h")
-# print(dir(Student))
-print(Student.__doc__)
+# class Student:
+#     '''deepak'''
+#     school='SHSS'
+#     def showdetails():
+#         print("h")
+# # print(dir(Student))
+# print(Student.__doc__)
+# s=Student
+# print(s)
+# print(print("hello"))
+# class Student:
+#     pass
+# print(id(Student))
+# obj=Student()
+# print(id(obj))
+# obj1=Student()
+# print(id(obj1))
+# class student:
+#     def __init__(self):
+#         print("constructor called")
+# obj=student
+# obj=student()
+# class student:
+#     def __init__(self):
+#         print("constructor called")
+#         print(id(self))
+# obj=student
+# obj=student()
+# print(id(obj))
+# class Student:
+#     def __init__(self,name,quali):
+#        self.n=name
+#        self.q=quali
+# obj=Student("Deepak","Cricketer")
+# obj1=Student("Deepak","Cricketer")
+# print(id(obj.n))
+# print(id(obj1.n))
+
