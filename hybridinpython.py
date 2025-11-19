47552c919237627ff39bd6333fc91dc29b345972
Deepakab17 <deepaknab17@gmail.com>
2025-11-19 23:57:35 +0530

    class inheritence

---
+++ hybridinpython.py
@@ -0,0 +1,18 @@
+# class A:
+# #     def car(self):
+# #         print("car from A")
+
+# # class B(A):
+# #     def watch(self):
+# #         print("watch from B")
+
+# # class C(A):
+# #     def house(self):
+# #         print("house from B")
+# # class D(B,C):
+# #     def shirt(self):
+# #         print("shirt from D")
+
+# # obj=D()
+# # obj.shirt()
+
